c67817a0f32a67e011b9b73097c2d9ab6d68bfea
tademarozsak <65410328+tademarozsak@users.noreply.github.com>
2022-06-16 21:34:21 +0100

    Update receiver.py

--- assignment6/receiver.py
+++ assignment6/receiver.py
@@ -18,6 +18,7 @@ import time
 class tictac(Packet):
     name = "tictac"
     fields_desc = [ IntField("user_input", 0),
+                    IntField("is_valid", 0),
                     IntField("field0", 0),
                     IntField("field1", 0),
                     IntField("field2", 0),
@@ -26,10 +27,28 @@ class tictac(Packet):
                     IntField("field5", 0),
                     IntField("field6", 0),
                     IntField("field7", 0),
-                    IntField("field8", 0),
-                    IntField("is_valid", 0)]
+                    IntField("field8", 0)]
 
 bind_layers(Ether, tictac, type=0x1234)
+
+
+
+def get_response(iface):
+    resp2 = sniff(filter = "ether dst 00:04:00:00:00:00", iface=iface,count = 2, timeout=10)
+    return resp2[1]
+
+
+
+def output_table(hdr):
+    matrix =[hdr.field0,hdr.field1,hdr.field2,hdr.field3,hdr.field4,hdr.field5,hdr.field6,hdr.field7,hdr.field8]
+    matrix = ['•' if item == 0 else 'o' if item == 2 else 'x' if item == 1 else item for item in matrix]
+    matrix = [matrix[0:3],matrix[3:6],matrix[6:9]]
+
+    for v in matrix:
+        v1, v2, v3 = v
+        print ("{:<1} {:<1} {:<1}".format(v1,v2,v3))
+
+    print ('\n\n')
 
 
 
@@ -41,25 +60,18 @@ def main():
     while True:
         time.sleep(0.5)
         try:
-            resp2 = sniff(filter = "ether dst 00:04:00:00:00:00", iface=iface,count = 2, timeout=10)
-            resp = resp2[1]
+            resp = get_response(iface)
             if resp:
-                var1=resp[tictac]
-                if var1:
-                    #resp.show()
-                    table = [[var1.field0,var1.field1,var1.field2],[var1.field3,var1.field4,var1.field5],[var1.field6,var1.field7,var1.field8]]
-                    matrix =[var1.field0,var1.field1,var1.field2,var1.field3,var1.field4,var1.field5,var1.field6,var1.field7,var1.field8]
-                    matrix = ['•' if item == 0 else 'o' if item == 2 else 'x' if item == 1 else item for item in matrix]
-                    matrix = [matrix[0:3],matrix[3:6],matrix[6:9]]
-                    for v in matrix:
-                    	v1, v2, v3 = v
-                    	print ("{:<1} {:<1} {:<1}".format(v1,v2,v3))
-                    	
-                    print ('\n\n')
-                    # print(var1.is_valid)
-                    
+                hdr = resp[tictac]
+                if hdr:
+                    if hdr.is_valid == 1:
+                        #resp.show()
+                        output_table(hdr)
+                        # print(hdr.is_valid)
+                    else:
+                        print("Invalid input (field already occupied); enter a different number")
                 else:
-                    print("cannot find P4calc header in the packet")
+                    print("Cannot find tictac header in the packet")
             else:
                 print("Didn't receive response")
 
