9f14999067930dc2e0d05748560cb395d468cad9
tademarozsak <65410328+tademarozsak@users.noreply.github.com>
2022-06-16 17:03:21 +0100

    Update receiver.py

--- assignment6/receiver.py
+++ assignment6/receiver.py
@@ -26,7 +26,8 @@ class tictac(Packet):
                     IntField("field5", 0),
                     IntField("field6", 0),
                     IntField("field7", 0),
-                    IntField("field8", 0)]
+                    IntField("field8", 0),
+                    IntField("is_valid", 0)]
 
 bind_layers(Ether, tictac, type=0x1234)
 
@@ -40,10 +41,31 @@ def main():
     while True:
         time.sleep(0.5)
         try:
-            resp = sniff(filter = "ether dst 00:04:00:00:00:00", iface=iface,count = 2, timeout=10)
-            print(resp)
+            resp2 = sniff(filter = "ether dst 00:04:00:00:00:00", iface=iface,count = 2, timeout=10)
+            resp = resp2[1]
+            if resp:
+                var1=resp[tictac]
+                if var1:
+                    #resp.show()
+                    table = [[var1.field0,var1.field1,var1.field2],[var1.field3,var1.field4,var1.field5],[var1.field6,var1.field7,var1.field8]]
+                    matrix =[var1.field0,var1.field1,var1.field2,var1.field3,var1.field4,var1.field5,var1.field6,var1.field7,var1.field8]
+                    matrix = ['•' if item == 0 else 'o' if item == 2 else 'x' if item == 1 else item for item in matrix]
+                    matrix = [matrix[0:3],matrix[3:6],matrix[6:9]]
+                    for v in matrix:
+                    	v1, v2, v3 = v
+                    	print ("{:<1} {:<1} {:<1}".format(v1,v2,v3))
+                    	
+                    print ('\n\n')
+                    # print(var1.is_valid)
+                    
+                else:
+                    print("cannot find P4calc header in the packet")
+            else:
+                print("Didn't receive response")
+
         except Exception as error:
-            print(error)
+            #print(error)
+            pass
 
 
 if __name__ == '__main__':
