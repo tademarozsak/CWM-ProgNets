8962b30949fe71720f330e90060de01249d3b5d9
tademarozsak <65410328+tademarozsak@users.noreply.github.com>
2022-06-16 12:20:00 +0100

    Update sender.py

--- assignment6/sender.py
+++ assignment6/sender.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+#SENDER
 
 import argparse
 import sys
@@ -13,57 +14,25 @@ from scapy.all import Ether, StrFixedLenField, XByteField, IntField
 from scapy.all import bind_layers
 import readline
 
-class P4calc(Packet):
-    name = "P4calc"
-    fields_desc = [ StrFixedLenField("P", "P", length=1),
-                    StrFixedLenField("Four", "4", length=1),
-                    XByteField("version", 0x01),
-                    StrFixedLenField("op", "+", length=1),
-                    IntField("operand_a", 0),
-                    IntField("operand_b", 0),
-                    IntField("result", 0xDEADBABE)]
+class tictac(Packet):
+    name = "tictac"
+    fields_desc = [ IntField("user_input", 0),
+                    IntField("field0", 0),
+                    IntField("field1", 0),
+                    IntField("field2", 0),
+                    IntField("field3", 0),
+                    IntField("field4", 0),
+                    IntField("field5", 0),
+                    IntField("field6", 0),
+                    IntField("field7", 0),
+                    IntField("field8", 0)]
 
-bind_layers(Ether, P4calc, type=0x1234)
+bind_layers(Ether, tictac, type=0x1234)
 
-class NumParseError(Exception):
-    pass
-
-class OpParseError(Exception):
-    pass
-
-class Token:
-    def __init__(self,type,value = None):
-        self.type = type
-        self.value = value
-
-def num_parser(s, i, ts):
-    pattern = "^\s*([0-9]+)\s*"
-    match = re.match(pattern,s[i:])
-    if match:
-        ts.append(Token('num', match.group(1)))
-        return i + match.end(), ts
-    raise NumParseError('Expected number literal.')
-
-
-def op_parser(s, i, ts):
-    pattern = "^\s*([-+&|^])\s*"
-    match = re.match(pattern,s[i:])
-    if match:
-        ts.append(Token('num', match.group(1)))
-        return i + match.end(), ts
-    raise NumParseError("Expected binary operator '-', '+', '&', '|', or '^'.")
-
-
-def make_seq(p1, p2):
-    def parse(s, i, ts):
-        i,ts2 = p1(s,i,ts)
-        return p2(s,i,ts2)
-    return parse
 
 
 def main():
 
-    p = make_seq(num_parser, make_seq(op_parser,num_parser))
     s = ''
     iface = 'eth0'
 
@@ -72,15 +41,15 @@ def main():
         if s == "quit":
             break
         print(s)
-        try:
-            i,ts = p(s,0,[])
-            pkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / P4calc(op=ts[1].value,
-                                              operand_a=int(ts[0].value),
-                                              operand_b=int(ts[2].value))
-            pkt = pkt/' '
 
-            pkt.show()
-            resp = srp1(pkt, iface=iface, timeout=1, verbose=False)
+        try:
+            if isinstance(s, int) and s>=1 and s<=10:            
+                pkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / tictac(user_input=s)
+                pkt = pkt/' '
+                pkt.show()
+                resp = srp1(pkt, iface=iface, timeout=1, verbose=False)
+            else:
+                print("Invalid input. Enter a number between 1 and 9")
         except Exception as error:
             print(error)
 
