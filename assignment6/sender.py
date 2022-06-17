353ad512de7011bc2d6b53763ccbc29186e62f5a
tademarozsak <65410328+tademarozsak@users.noreply.github.com>
2022-06-17 12:23:40 +0100

    Update sender.py

--- assignment6/sender.py
+++ assignment6/sender.py
@@ -42,25 +42,37 @@ def main():
         s = input('> ')
         if s == "quit":
             break
-        print(s)
-
+        #print(s)
+        
+        #HELP command:
+        if s.lower() == 'help':
+            print("You have to enter a number between 1 and 9 corresponding to the following fields:\n")	
+            for v in [[1,2,3],[4,5,6],[7,8,9]]:
+                v1, v2, v3 = v
+                print ("{:<1} {:<1} {:<1}".format(v1,v2,v3))
+            print("\nIf the fields is aleady occupied or the input is invalid, you will be asked to enter another one.\nFor results and outputs please use the display on the receiving end.")
         try:
-            if s == "new" or s == "reset":
+            #Check if new game should start:
+            if s.lower() == "new" or s.lower() == "reset" or s.lower() == "restart" or s.lower() == "start":
                 pkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / tictac(new_game=1)
                 pkt = pkt/' '
-                pkt.show()
+                #pkt.show()
                 resp = srp1(pkt, iface=iface, timeout=1, verbose=False)
+            #Otherwise check if input is within range, and if so, forward it
             else:
                 s = int(s)
+                s -= 1
                 if isinstance(s, int) and s>=0 and s<=8:            
                     pkt = Ether(dst='00:04:00:00:00:00', type=0x1234) / tictac(user_input=s, new_game=0)
                     pkt = pkt/' '
-                    pkt.show()
+                    #pkt.show()
                     resp = srp1(pkt, iface=iface, timeout=1, verbose=False)
+                #If use input is outside allowed range, give warning:
                 else:
-                    print("Invalid input. Enter a number between 0 and 9")
+                    print("Invalid input. Enter a number between 1 and 9")
         except Exception as error:
-            print(error)
+            #print(error)
+            pass
 
 
 if __name__ == '__main__':
